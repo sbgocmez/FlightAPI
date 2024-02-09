c0c46b8de9c576e33df5a12950ca50f3cd06ab2a
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2024-02-09 10:54:01 +0300

    project startup

---
+++ src/test/java/com/sbgocmez/FlightAPI/FlightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbgocmez.FlightAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
